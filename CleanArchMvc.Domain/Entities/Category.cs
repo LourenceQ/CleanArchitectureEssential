8a61586d01a6cb2e7a5c3da9d7fa501509ff13e0
LawrenceQ <lawrenceqf@gmail.com>
2022-11-22 22:41:16 -0300

    Implementando validação de entidades

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -1,8 +1,30 @@
-﻿namespace CleanArchMvc.Domain.Entities;
+﻿using CleanArchMvc.Domain.Validation;
 
-public class Category
+namespace CleanArchMvc.Domain.Entities;
+
+public sealed class Category
 {
-    public int Id { get; set; }
-    public string  Name { get; set; }
-    public ICollection<Product> Products{ get; set; }
+    public int Id { get; private set; }
+    public string Name { get; private set; }
+    public ICollection<Product> Products { get; private set; }
+
+    public Category(string name)
+    {
+        ValidateDomain(name);
+    }
+    public Category(int id, string name)
+    {
+        DomainExceptionValidation.When(id < 0, "Id inválido.");
+        Id = id;
+        ValidateDomain(name);
+    }
+
+    private void ValidateDomain(string name)
+    {
+        DomainExceptionValidation.When(string.IsNullOrEmpty(name)
+            , "Nome inválido. Nome é obrigatório");
+
+        DomainExceptionValidation.When(name.Length < 3
+            , "Nome inválido. Mínimo de 3 caracteres.");
+    }
 }
