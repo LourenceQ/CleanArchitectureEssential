52464cc7a8811218e1b39884b542b56f57e05eae
LawrenceQ <lawrenceqf@gmail.com>
2022-11-22 22:24:43 -0300

    Implementando entidades e criando solution folders

---
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace CleanArchMvc.Domain.Entities;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string  Name { get; set; }
+    public ICollection<Product> Products{ get; set; }
+}
---
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanArchMvc.Domain.Entities;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+    public string Image { get; set; }
+}
