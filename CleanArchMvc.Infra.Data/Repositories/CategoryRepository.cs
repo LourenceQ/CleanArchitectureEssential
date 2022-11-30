92b27da151082dcc4969337dbd8b9e0e3b404d81
LawrenceQ <lawrenceqf@gmail.com>
2022-11-30 06:54:38 -0300

    Implementando repositórios

---
+++ CleanArchMvc.Infra.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,47 @@
+﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchMvc.Infra.Data.Repositories;
+
+public class CategoryRepository : ICategoryRepository
+{
+    private readonly ApplicationDbContext _contextCategory;
+
+    public CategoryRepository(ApplicationDbContext contextCategory)
+    {
+        _contextCategory = contextCategory;
+    }
+
+    public async Task<Category> CreateAsync(Category category)
+    {
+        _contextCategory.Add(category);
+        await _contextCategory.SaveChangesAsync();
+        return category;
+    }
+
+    public async Task<Category> GetByIdAsync(int? id)
+    {
+        return await _contextCategory.Categories.FindAsync(id);
+    }
+
+    public async Task<IEnumerable<Category>> GetCategoriesAsync()
+    {
+        return await _contextCategory.Categories.ToListAsync();
+    }
+
+    public async Task<Category> RemoveAsync(Category category)
+    {
+        _contextCategory.Remove(category);
+        await _contextCategory.SaveChangesAsync();
+        return category;
+    }
+
+    public async Task<Category> UpdateAsync(Category category)
+    {
+        _contextCategory.Update(category);
+        await _contextCategory.SaveChangesAsync();
+        return category;
+    }
+}
