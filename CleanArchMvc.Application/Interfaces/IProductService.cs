8b567a7ade1741562552eb8d90fc998a1dc50ae6
LawrenceQ <lawrenceqf@gmail.com>
2023-01-02 19:24:57 -0300

    Implementando interface e service de product

---
+++ CleanArchMvc.Application/Interfaces/IProductService.cs
@@ -0,0 +1,11 @@
+﻿using CleanArchMvc.Application.DTOs;
+
+namespace CleanArchMvc.Application.Interfaces;
+
+public interface IProductService
+{Task<IEnumerable<ProductDTO>> GetProducts();
+    Task<ProductDTO> GetById(int? id);
+    Task Add(ProductDTO productDTO);
+    Task Update(ProductDTO productDTO);
+    Task Remove(int? id);
+}
---
+++ CleanArchMvc.Application/Services/ProductService.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+
+namespace CleanArchMvc.Application.Services;
+
+public class ProductService : IProductRepository
+{
+    private readonly IProductRepository _repository;
+    private readonly IMapper _mapper;
+
+    public ProductService(IMapper mapper
+        , IProductRepository repository)
+    {
+        _mapper = mapper;
+        _repository = repository;
+    }
+
+    public Task<Product> CreateAsync(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product> GetByIdAsync(int? id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product> GetProductCategoryAsync(int? id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Product>> GetProductsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product> RemoveAsync(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product> UpdateAsync(Product product)
+    {
+        throw new NotImplementedException();
+    }
+}
