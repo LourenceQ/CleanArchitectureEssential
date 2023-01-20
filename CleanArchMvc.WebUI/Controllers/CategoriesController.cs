02a1200972d56d6b6128d47519c859dc4e21cef1
LawrenceQ <lawrenceqf@gmail.com>
2023-01-19 21:32:49 -0300

    Implementando controller e view Delete

--- CleanArchMvc.WebUI/Controllers/CategoriesController.cs
+++ CleanArchMvc.WebUI/Controllers/CategoriesController.cs
@@ -49,4 +49,23 @@ public class CategoriesController : Controller
 
         return View(categoryDto);
     }
+
+    [HttpGet()]
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null) return NotFound();
+
+        var categoryDto = await _categoryService.GetById(id);
+
+        if (categoryDto == null) return NotFound();
+
+        return View(categoryDto);
+    }
+
+    [HttpPost(), ActionName("Delete")]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        await _categoryService.Remove(id);
+        return RedirectToAction("Index");
+    }
 }
