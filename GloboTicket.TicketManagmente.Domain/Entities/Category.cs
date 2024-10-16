a78c32eb9dd25f181ecf7c0e2dca635fe254349a
LourenceQ <lawrenceqf@gmail.com>
2024-10-15 21:35:31 -0300

    implementando auditable entity

--- GloboTicket.TicketManagmente.Domain/Entities/Category.cs
+++ GloboTicket.TicketManagmente.Domain/Entities/Category.cs
@@ -1,9 +1,11 @@
-﻿namespace GloboTicket.TicketManagmente.Domain.Entities
+﻿using GloboTicket.TicketManagmente.Domain.Common;
+
+namespace GloboTicket.TicketManagmente.Domain.Entities
 {
-    public class Category
+    public class Category : AuditableEntity
     {
         public Guid CategoryId { get; set; }
         public string Name { get; set; } = string.Empty;
-        public ICollection<Eevent>?  Events{ get; set; }
+        public ICollection<Order>?  Events{ get; set; }
     }
 }
