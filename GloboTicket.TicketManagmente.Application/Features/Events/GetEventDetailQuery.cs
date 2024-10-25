10180c73a8d18b3fb28dd6a87946ba06d2423acc
LourenceQ <lawrenceqf@gmail.com>
2024-10-24 21:01:39 -0300

    implementando dtos de Event

---
+++ GloboTicket.TicketManagmente.Application/Features/Events/GetEventDetailQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace GloboTicket.TicketManagment.Application.Features.Events
+{
+    public class GetEventDetailQuery : IRequest<EventDetailVm>
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ GloboTicket.TicketManagmente.Application/Features/Profile/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using GloboTicket.TicketManagment.Application.Features.Events;
+using GloboTicket.TicketManagmente.Domain.Entities;
+
+namespace GloboTicket.TicketManagment.Application.Features.Profile
+{
+    public class MappingProfile : AutoMapper.Profile
+    {
+        protected MappingProfile()
+        {
+            CreateMap<Event, EventListVm>().ReverseMap();
+        }
+    }
+}
