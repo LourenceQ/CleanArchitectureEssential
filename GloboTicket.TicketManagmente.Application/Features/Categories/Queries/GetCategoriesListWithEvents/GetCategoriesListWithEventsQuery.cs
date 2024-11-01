57a22f1193bb605907d0305344f0a9b47a6a0fb7
LourenceQ <lawrenceqf@gmail.com>
2024-11-01 07:35:22 -0300

    implementando GetCategoriesListWithEventsQueryHandler

---
+++ GloboTicket.TicketManagmente.Application/Features/Categories/Queries/GetCategoriesListWithEvents/GetCategoriesListWithEventsQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace GloboTicket.TicketManagment.Application.Features.Categories.Queries.GetCategoriesListWithEvents
+{
+    public class GetCategoriesListWithEventsQuery : IRequest<List<CategoryEventListVm>>
+    {
+        public bool IncludeHistory { get; set; }
+    }
+}
---
+++ GloboTicket.TicketManagmente.Application/Features/Categories/Queries/GetCategoriesListWithEvents/GetCategoriesListWithEventsQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagment.Application.Contracts.Persistance;
+using MediatR;
+
+namespace GloboTicket.TicketManagment.Application.Features.Categories.Queries.GetCategoriesListWithEvents
+{
+    public class GetCategoriesListWithEventsQueryHandler : IRequestHandler<GetCategoriesListWithEventsQuery
+        , List<CategoryEventListVm>>
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public GetCategoriesListWithEventsQueryHandler(ICategoryRepository eventRepository, IMapper mapper)
+        {
+            _categoryRepository = eventRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<CategoryEventListVm>> Handle(GetCategoriesListWithEventsQuery request, CancellationToken cancellationToken)
+        {
+            var list = await _categoryRepository.GetCategoriesWithEvents(request.IncludeHistory);
+            
+            return _mapper.Map<List<CategoryEventListVm>>(list);
+        }
+    }
+}
