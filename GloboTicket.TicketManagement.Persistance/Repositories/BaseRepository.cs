2dec316c611b4b355d121e6d6ac1ef39d6953cb9
LourenceQ <lawrenceqf@gmail.com>
2024-11-11 19:48:34 -0300

    implementando repository base

---
+++ GloboTicket.TicketManagement.Persistance/Repositories/BaseRepository.cs
@@ -0,0 +1,33 @@
+﻿using GloboTicket.TicketManagment.Application.Contracts.Persistance;
+
+namespace GloboTicket.TicketManagement.Persistance.Repositories;
+
+public class BaseRepository<T> : IAsyncRepository<T> where T : class
+{
+    protected readonly GloboTicketDbContext _dbContext;
+
+    public Task<T> AddAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> DeleteAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> GetByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<T>> ListAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> UpdateAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+}
